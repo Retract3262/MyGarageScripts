d876bdde597cc6e3707c0fd1c3c28b5aa2810dd8
Retract3262 <131391155+Retract3262@users.noreply.github.com>
2023-09-30 22:12:58 -0400

    Create ExtractB64PNG.py

---
+++ ExtractB64PNG.py
@@ -0,0 +1,33 @@
+import base64
+import os
+import re
+
+PATH = r"" 
+# Path to the directory containing files to extract PNG images from
+# C:\Users\<USER>\AppData\LocalLow\Viking\My Garage\save1 as an example
+
+if __name__ == "__main__":
+
+    png_list = []
+    png_counter = 1
+
+    for f in os.listdir(PATH):
+
+        if os.path.isdir(os.path.join(PATH, f)):
+            continue
+
+        with open(os.path.join(PATH, f), "rb") as fp:
+            matches = re.findall(b'iVBORw0KGgo[a-zA-Z0-9+/=]*', fp.read())
+            for match in matches:
+                if len(match) > 4:
+                    png_list.append(match)
+
+    os.makedirs(os.path.join(PATH, "images"), exist_ok=True)
+
+    for png in png_list:
+        png_data = base64.b64decode(png)
+        png_counter += 1
+        with open(os.path.join(PATH, "images", f"{png_counter}.png"), "wb") as out_fp:
+            out_fp.write(png_data)
+
+    print(f"Found {png_counter} total png images")
